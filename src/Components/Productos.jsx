d4321781aaed87c31b043692bc754da8208cb375
samue1975 <newtmassmiley@gmail.com>
2024-06-17 21:23:25 -0400

    Se modifico el prop de productos en Catalogo y se reutilizo el componente por cada categoria

--- src/Components/Productos.jsx
+++ src/Components/Productos.jsx
@@ -1,22 +1,19 @@
 import { TbListDetails } from "react-icons/tb";
 
-const Productos = () => {
+const Productos = ({ foto, titulo, descripcion }) => {
   return (
     <div className="border rounded-2xl flex flex-col w-52 h-60 shadow cursor-pointer bg-[#f6f6f6] hover:bg-[#393939] hover:text-white flex justify-around">
       <div className="h-40 flex justify-center">
-        <img
-          className="h-full"
-          src="https://cdn.pixabay.com/photo/2013/07/13/13/46/computer-161501_1280.png"
-        />
+        <img className="h-full" src={foto} />
       </div>
       <div className="flex justify-between items-end p-4">
         <div className="flex flex-col flex-wrap w-full">
-          <h1 className="text-base">Servidor Rack</h1>
-          <p className="text-sm">Equipo</p>
+          <h1 className="text-base">{titulo}</h1>
+          <p className="text-sm">{descripcion}</p>
         </div>
-        <div className="hover:text-[#d2d2d2]">
+        <button className="hover:text-[#d2d2d2]">
           <TbListDetails />
-        </div>
+        </button>
       </div>
     </div>
   );
--- src/Pages/Catalogo.jsx
+++ src/Pages/Catalogo.jsx
@@ -32,18 +32,48 @@ const Catalogo = () => {
         </div>
       </div>
       <div className="pt-8 flex-wrap gap-6 grid grid-cols-5">
-        <Productos />
-        <Productos />
-        <Productos />
-        <Productos />
-        <Productos />
-        <Productos />
-        <Productos />
-        <Productos />
-        <Productos />
-        <Productos />
-        <Productos />
-        <Productos />
+        <Productos
+          foto={
+            "https://pngimg.com/uploads/server/server_PNG23.png"
+          }
+          titulo={"Servidor Rack"}
+          descripcion={"Equipo"}
+        />
+        <Productos
+          foto={
+            "https://www.pngall.com/wp-content/uploads/10/Motor-PNG-File.png"
+          }
+          titulo={"Motor"}
+          descripcion={"Componente"}
+        />
+        <Productos
+          foto={
+            "https://gmb.net/wp-content/uploads/2021/09/DSC05370-1-e1631634148806-450x346.png"
+          }
+          titulo={"Correa de Dist"}
+          descripcion={"Parte"}
+        />
+        <Productos
+          foto={
+            "https://laminas.com.mx/wp-content/uploads/2019/11/varilla-corrugada-300x223.png"
+          }
+          titulo={"Barra de Acero"}
+          descripcion={"Materia Prima"}
+        />
+        <Productos
+          foto={
+            "https://www.pngall.com/wp-content/uploads/5/Motor-Oil-PNG-High-Quality-Image.png"
+          }
+          titulo={"Aceite de Motor"}
+          descripcion={"Consumible"}
+        />
+        <Productos
+          foto={
+            "https://www.unionferretera.com/37127-large_default/llave-ajustable-irimo.jpg"
+          }
+          titulo={"Llave Inglesa"}
+          descripcion={"Herramienta"}
+        />
       </div>
     </div>
   );

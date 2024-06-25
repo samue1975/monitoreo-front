7afba4896fcb16abff53031cfb4103c0b2350514
samue1975 <newtmassmiley@gmail.com>
2024-06-25 15:30:40 -0400

    Se añadio el apartado de detalles en el catalogo

--- src/Pages/Catalogo.jsx
+++ src/Pages/Catalogo.jsx
@@ -2,8 +2,29 @@ import { IoIosAdd } from "react-icons/io";
 import Busqueda from "../Components/Busqueda";
 import Productos from "../Components/Productos";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
-const Catalogo = () => {
+const Catalogo = ({
+  Nombre,
+  Cantidad,
+  CodOaci,
+  CodE,
+  CodFg,
+  CodHi,
+  CodJk,
+  CodSu,
+  CodLmnop,
+  Descrip,
+  CodBarra,
+  CodSistema,
+  Codigo,
+}) => {
+  const [open, setOpen] = useState(false);
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="col-span-5 pt-4 px-8">
       {/* APARTADO DE BUSQUEDA Y DE BOTONES */}
@@ -43,6 +64,7 @@ const Catalogo = () => {
           foto={"https://pngimg.com/uploads/server/server_PNG23.png"}
           titulo={"Servidor Rack"}
           descripcion={"Equipo"}
+          open={toggleMenu}
         />
         <Productos
           foto={
@@ -50,6 +72,7 @@ const Catalogo = () => {
           }
           titulo={"Motor"}
           descripcion={"Componente"}
+          open={toggleMenu}
         />
         <Productos
           foto={
@@ -57,6 +80,7 @@ const Catalogo = () => {
           }
           titulo={"Correa de Dist"}
           descripcion={"Parte"}
+          open={toggleMenu}
         />
         <Productos
           foto={
@@ -64,6 +88,7 @@ const Catalogo = () => {
           }
           titulo={"Barra de Acero"}
           descripcion={"Materia Prima"}
+          open={toggleMenu}
         />
         <Productos
           foto={
@@ -71,6 +96,7 @@ const Catalogo = () => {
           }
           titulo={"Aceite de Motor"}
           descripcion={"Consumible"}
+          open={toggleMenu}
         />
         <Productos
           foto={
@@ -78,7 +104,85 @@ const Catalogo = () => {
           }
           titulo={"Llave Inglesa"}
           descripcion={"Herramienta"}
+          open={toggleMenu}
         />
+      </div>
+      <div
+        className={` ${
+          open ? "block" : "hidden"
+        } border rounded-2xl flex flex-col w-2/4  top-0 bottom-0 left-0 right-0 m-auto shadow bg-white justify-around fixed`}
+      >
+        {/* FLECHA PARA IR HACIA ATRAS */}
+        <span className="px-10">
+          <BiArrowBack
+            onClick={toggleMenu}
+            className="cursor-pointer text-xl text-[#292929] size-4"
+          />
+        </span>
+        <div className="h-40 flex justify-center">
+          <img
+            className="h-full"
+            src={
+              "https://laminas.com.mx/wp-content/uploads/2019/11/varilla-corrugada-300x223.png"
+            }
+          />
+        </div>
+        <div className="flex justify-between items-end p-4">
+          <div className="flex flex-col flex-wrap w-full px-10">
+            <h1 className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Nombre:</p>
+              {Nombre}
+            </h1>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Cantidad:</p>
+              {Cantidad}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Código OACI:</p>
+              {CodOaci}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Identificación:</p>
+              {CodE}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Departamento:</p>
+              {CodFg}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Sector De Investigación:</p>
+              {CodHi}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Línea de Investigación:</p>
+              {CodJk}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Proveedores:</p>
+              {CodSu}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">ID Cronológica:</p>
+              {CodLmnop}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Descripción:</p>
+              {Descrip}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Código de Barra:</p>
+              {CodBarra}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Código del Sistema:</p>
+              {CodSistema}
+            </p>
+            <p className="text-base flex flex-wrap gap-4">
+              <p className="font-semibold">Código:</p>
+              {Codigo}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );

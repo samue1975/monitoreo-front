158a1a9dd0bdf4b6d9ba4145714fba998bbcea30
samue1975 <newtmassmiley@gmail.com>
2024-06-28 13:54:30 -0400

    se actualizo un error

--- src/Components/Productos.jsx
+++ src/Components/Productos.jsx
@@ -1,10 +1,8 @@
 import { TbListDetails } from "react-icons/tb";
 import { Link } from "react-router-dom";
-import { MdDelete } from "react-icons/md";
-
 
 // eslint-disable-next-line react/prop-types
-const Productos = ({ foto, titulo, descripcion, idCodProd, deleteData }) => {
+const Productos = ({ foto, titulo, descripcion, idCodProd }) => {
   return (
     <div className="border rounded-2xl flex flex-col w-52 h-60 shadow cursor-pointer bg-[#f6f6f6] hover:bg-[#393939] hover:text-white justify-around">
       <div className="h-40 flex justify-center">
@@ -19,9 +17,6 @@ const Productos = ({ foto, titulo, descripcion, idCodProd, deleteData }) => {
           <Link to={`/Catalogo/${idCodProd}`}>
             <TbListDetails />
           </Link>
-        </button>
-        <button className="hover:text-[#d2d2d2]" onClick={deleteData(idCodProd)}>
-          <MdDelete />
         </button>
       </div>
     </div>

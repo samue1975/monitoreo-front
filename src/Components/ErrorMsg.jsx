497a252d47dff5d25663c5d7539acff595f2d421
samuel <samuelsantana3330@gmail.com>
2024-07-19 15:11:16 -0400

    modificaciones de catalogo

--- src/Components/ErrorMsg.jsx
+++ src/Components/ErrorMsg.jsx
@@ -1,7 +1,8 @@
-const ErrorMsg = ({ display }) => {
+// eslint-disable-next-line react/prop-types
+const ErrorMsg = ({ text }) => {
   return (
-    <div className={`text-end text-[#ff0000ad] text-xs ${display}`}>
-      *Completa los campos vacíos*
+    <div className={`text-end text-[#ff0000ad] text-xs`}>
+      {text}
     </div>
   );
 };

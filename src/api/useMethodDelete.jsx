e51cf2b3f924603203aaa33b400757b894e29371
fernandzxc <alifernandopinto@gmail.com>
2024-08-16 12:50:24 -0400

    actualizacion

--- src/api/useMethodDelete.jsx
+++ src/api/useMethodDelete.jsx
@@ -1,32 +1,45 @@
-import { useState, useCallback } from 'react';
+// import { useState, useCallback } from 'react';
+
+// const useDelete = (url) => {
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [error, setError] = useState(null);
+//     const [data, setData] = useState(null);
+
+//     const deleteData = useCallback(async (id) => {
+//         setIsLoading(true);
+//         setError(null);
+
+//         try {
+//             const response = await fetch(`${url}/${id}`, {
+//                 method: 'DELETE',
+//             });
+
+//             if (!response.ok) {
+//                 throw new Error('Failed to delete data');
+//             }
+//             const result = await response.json();
+//             setData(result);
+//         } catch (err) {
+//             setError(err.message);
+//         } finally {
+//             setIsLoading(false);
+//         }
+//     }, [url]);
+
+//     return { isLoading, error, data, deleteData };
+// };
+
+// export default useDelete;
+
+import {useState, useCallback} from 'react';
 
 const useDelete = (url) => {
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsloading] = useState(false);
     const [error, setError] = useState(null);
     const [data, setData] = useState(null);
 
     const deleteData = useCallback(async (id) => {
-        setIsLoading(true);
-        setError(null);
-
-        try {
-            const response = await fetch(`${url}/${id}`, {
-                method: 'DELETE',
-            });
-
-            if (!response.ok) {
-                throw new Error('Failed to delete data');
-            }
-            const result = await response.json();
-            setData(result);
-        } catch (err) {
-            setError(err.message);
-        } finally {
-            setIsLoading(false);
-        }
-    }, [url]);
-
-    return { isLoading, error, data, deleteData };
-};
-
-export default useDelete;
+        
+    })
+}
+ 
